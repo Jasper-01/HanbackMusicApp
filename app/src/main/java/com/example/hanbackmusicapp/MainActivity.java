d75bc1a1c498ed22978db1dec4b125c9516edb5b
Jasper <jasperyap2001@gmail.com>
2024-05-21 15:26:17 +0900

    initial commit

--- app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
+++ app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
@@ -18,64 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    private TextView timerDisplay;
-    private Boolean timerIsRunning;
-    private Button playPauseBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        timerDisplay = findViewById(R.id.timerDisplay);
-        playPauseBtn = findViewById(R.id.playPauseBtn);
-
-        playPauseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(timerIsRunning){
-                    playPauseBtn.setBackgroundResource(R.drawable.ic_baseline_pause_icon);
-                    timerPause();
-                } else{
-                    playPauseBtn.setBackgroundResource(R.drawable.ic_baseline_play_icon);
-                    timerStart();
-                }
-            }
-        });
     }
 
-    public void timerStart(){
-        timerIsRunning = true;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                startTimerNative(timerDisplay);
-            }
-        });
-    }
-
-    public void timerPause(){
-        timerIsRunning = false;
-        pauseTimerNative();
-    }
-
-    public void timerReset(){
-        timerIsRunning = false;
-        resetTimerNative();
-        timerDisplay.setText(R.string.timerRstDisplayVal);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(timerIsRunning){
-            timerPause();
-        }
-    }
-
-    public native void startTimerNative(View textView);
-    public native void pauseTimerNative();
-    public native void resetTimerNative();
 }
