e4545d68dceb8e26e41d0d1bf75be674f128868d
Jasper <jasperyap2001@gmail.com>
2024-05-27 10:57:11 +0900

    Added variables into MainActivity.java file

--- app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
+++ app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
@@ -6,19 +6,34 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.hanbackmusicapp.databinding.ActivityMainBinding;
 
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
 
+    /* Tags */
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     /* variables */
+    private static final String SERVER_URL = "http://ec2-54-196-174-136.compute-1.amazonaws.com:3000/data"; // TODO: Update address
     private Boolean isRunning;
+
+    // Timer
     private Handler timerHandler;
     private Runnable timerRunnable;
     private long pausedTimeElapsed;  // Variable to store the paused elapsed time
+
+    // Database
+    private TextView resultTextView;
+    private JSONArray jsonArray;
+    private int currentIndex = 0;
+    private WebView webVideo;
 
     // Used to load the 'hanbackmusicapp' library on application startup.
     static {
