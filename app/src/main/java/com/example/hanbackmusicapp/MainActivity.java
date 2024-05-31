178cb9ec53fe891b77fc8ec2f60f09120f2dbeaf
Dogii <d0g11.7.100@gmail.com>
2024-06-01 03:22:12 +0900

    Database connection and mp3 player complete. Need Hanback testing. Notes: -SERVER_URL has been changed to not include the "/data" -Removed everything else not relating to database for ease of referencing - Play/pause button works - Need to resize the webView

--- app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
+++ app/src/main/java/com/example/hanbackmusicapp/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.hanbackmusicapp;
-// TODO: Sync timer with sound detection
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,10 +9,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.hanbackmusicapp.databinding.ActivityMainBinding;
-import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,45 +23,19 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import android.Manifest;
-
-/*
-TODO : Before starting app
-1. WiFi connection
-2. AWS activity
-3. Database activity
-4. Turning on server
-5. Check from browser
-6. Check if address is updated
-
-CHECK STATES DIPSHIT
- */
 public class MainActivity extends AppCompatActivity {
 
     /* Tags */
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    /* Permission codes */
-    private static final int PERMISSION_REQUEST_CODE = 1;
-
     /* variables */
-    private static final String SERVER_URL = "http://ec2-54-172-93-231.compute-1.amazonaws.com:3000/data"; // TODO: Update address
+    private static final String SERVER_URL = "http://ec2-54-172-93-231.compute-1.amazonaws.com:3000"; // TODO: Update address
     private Boolean isRunning;
-    private Boolean isMute;
-    private Boolean isVisible;
-
-    private long pausedTimeElapsed;  // Variable to store the paused elapsed time
 
     // Database
     private JSONArray jsonArray;
     private int currentIndex = 0;
     private WebView webVideo;
-
-    // Visualizer
-    private BarVisualizer mVisualizer;
-    private AudioRecord audioRecord;
-    private Thread audioThread;
-    private boolean isRecording = false;
 
     // Used to load the 'hanbackmusicapp' library on application startup.
     static {
@@ -92,41 +56,29 @@ public class MainActivity extends AppCompatActivity {
         ImageButton playPauseBtn = findViewById(R.id.playPauseButton);
         ImageButton prevBtn = findViewById(R.id.prevBtn);
         ImageButton nextBtn = findViewById(R.id.nextBtn);
-        ImageButton muteBtn = findViewById(R.id.muteBtn);
-        ImageButton visualBtn = findViewById(R.id.visualizerBtn);
         // WebView
         webVideo = findViewById(R.id.webVid);
-        // Visualizer
-        mVisualizer = findViewById(R.id.visualizer);
 
         /* Initialization */
-        isRunning = true;
-        isVisible = true;
-        Log.d("Initialization", "timer complete");
-        // webView
         webVideo.setWebViewClient(new WebViewClient());
         webVideo.getSettings().setJavaScriptEnabled(true);
         webVideo.setWebChromeClient(new WebChromeClient());
-        Log.d("Initialization", "webView complete");
         Log.d("Initialization", "ALL COMPLETED");
 
         // Database connection
         new FetchDataFromServerTask().execute();
-
-        // Visualizer start
-        //startAudioRecording();
 
         /* Button Click Listeners*/
         // play & pause Button
         playPauseBtn.setOnClickListener(view -> {
             if (isRunning) {
                 playPauseBtn.setImageResource(R.drawable.ic_baseline_play_icon);
-                webVideo.loadUrl("javascript:document.getElementById('audioPlayer').play();");
+                webVideo.loadUrl("javascript:document.getElementById('audioPlayer').pause();");
                 Log.d("Pause/Play", "Pausing");
                 isRunning = false;
             } else {
                 playPauseBtn.setImageResource(R.drawable.ic_baseline_pause_icon);
-                webVideo.loadUrl("javascript:document.getElementById('audioPlayer').pause();");
+                webVideo.loadUrl("javascript:document.getElementById('audioPlayer').play();");
                 Log.d("Pause/Play", "Playing");
                 isRunning = true;
             }
@@ -155,31 +107,6 @@ public class MainActivity extends AppCompatActivity {
                 playPauseBtn.setImageResource(R.drawable.ic_baseline_pause_icon);
             }
         });
-
-        // mute Button
-        muteBtn.setOnClickListener(view -> {
-            if (isMute){
-                isMute = false;
-                muteBtn.setImageResource(R.drawable.baseline_volume_on_icon);
-                //webVideo.loadUrl("javascript:unMuteVideo()");
-            } else{
-                isMute = true;
-                muteBtn.setImageResource(R.drawable.baseline_volume_mute_icon);
-                //webVideo.loadUrl("javascript:MuteVideo()");
-            }
-        });
-
-        // visualizer visibility Button
-        // TODO: add actual functions (to Jasper)
-        visualBtn.setOnClickListener(view -> {
-            if(isVisible){
-                isVisible = false;
-                visualBtn.setImageResource(R.drawable.baseline_visibility_off_visualizer_icon);
-            } else{
-                isVisible = true;
-                visualBtn.setImageResource(R.drawable.baseline_visibility_visualizer_icon);
-            }
-        });
     }
 
     /* Database functions */
@@ -191,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
             BufferedReader reader = null;
 
             try {
-                URL url = new URL(SERVER_URL);
+                URL url = new URL(SERVER_URL+"/data");
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
 
@@ -255,78 +182,23 @@ public class MainActivity extends AppCompatActivity {
 
             // Construct the HTML content with the dynamic video ID
             String htmlContent = "<html><head>" +
-                    "<style> body, html { height: 100%; margin: 0; } </style>" +
+                    "<style> body, html { height: 100%; margin: 0; }" +
+                    "audio { width: 100%; height: 100%; }" + // Ensures the audio element fills the WebView
+                    "</style>" +
                     "</head><body style=\"display:flex; align-items:center; justify-content:center; height:100%;\">" +
-                    "<audio controls id=\"audioPlayer\" style=\"max-width: 100%; width: 100%;\">" +
-                    "<source src=\"http://ec2-54-172-93-231.compute-1.amazonaws.com:3000/play/" + videoID + "\" type=\"audio/mpeg\">" +
+                    "<audio controls id=\"audioPlayer\">" +
+                    "<source src=\""+SERVER_URL+"/play/" + videoID + "\" type=\"audio/mpeg\">" +
                     "Your browser does not support the audio element.</audio></body></html>";
 
             // Load the HTML content into the WebView
             webVideo.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null);
-            isRunning = true;
-            isMute = true;
+            isRunning = false;
 
             // set texts
             titleDisplay.setText(title);
             channelNameDisplay.setText(channelName);
         } catch (JSONException e) {
             Log.e(TAG, "Error displaying current item: " + e.getMessage());
-        }
-    }
-
-    /* Visualizer functions */
-    private void startAudioRecording() {
-//        int sampleRate = 44100;
-//        int sampleRate = 22050;
-//        int sampleRate = 16000;
-        int sampleRate = 11025;
-
-//        int bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        int bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_8BIT);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSION_REQUEST_CODE);
-            return;
-        } else{
-//            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
-            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_8BIT, bufferSize);
-        }
-
-        audioThread = new Thread(() -> {
-            byte[] buffer = new byte[bufferSize];
-            audioRecord.startRecording();
-            isRecording = true;
-            while (isRecording) {
-                int read = audioRecord.read(buffer, 0, buffer.length);
-                if (read > 0) {
-                    mVisualizer.setRawAudioBytes(buffer);
-                }
-            }
-        });
-
-        audioThread.start();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        isRecording = false;
-        if (audioRecord != null) {
-            audioRecord.stop();
-            audioRecord.release();
-        }
-    }
-
-    /* Permission Request */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //startAudioRecording();
-            } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-            }
         }
     }
 }
